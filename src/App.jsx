79e0de6513f369520e058ee92725962443c78418
lololoco159159159 <lorenzorfjogos123@gmail.com>
2026-06-10 21:50:11 -0300

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1>Pensamento Computacional &amp; BNCC</h1>
-      <p>Hello World — ambiente React funcionando.</p>
+      <p>Hello World — ambiente React funcionando 2.0.</p>
     </div>
   )
 }
