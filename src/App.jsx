99fd9a8bbfb85e96596394f21f601a2c0fd777bf
lololoco159159159 <lorenzorfjogos123@gmail.com>
2026-06-10 21:43:48 -0300

    implementacao inicial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Pensamento Computacional &amp; BNCC</h1>
+      <p>Hello World — ambiente React funcionando.</p>
+    </div>
+  )
+}
+
+export default App
